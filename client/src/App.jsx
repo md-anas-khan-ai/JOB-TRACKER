4ff9ebd8a83e6c91e99f202e3a191d337bc2e7ec
md-anas-khan-ai <md.anaskhan.aiml@gmail.com>
2026-05-08 21:53:55 +0530

    feat: setup React Router and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-center pt-20 text-blue-600">
-        Job Tracker
-      </h1>
-
-      <p className="text-center mt-4 text-gray-600">
-        Track your job applications easily
-      </p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,5 @@
+function Signup() {
+  return <h1 className="text-4xl">Signup Page</h1>
+}
+
+export default Signup
